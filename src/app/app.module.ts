bcc5e5005aefd6ddbc56a90573924fbf1c7c673b
Benito Zefferino <42010637+bzefferino@users.noreply.github.com>
2022-09-17 14:19:55 -0600

    Initial Commit and Add Skeleton of Client
    Add angular material, routing, and default routing to /people as required by 1.a. Add a people service. Add a people component/page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { PeopleComponent } from './pages/people/people';
+
+// Instead of having the routes in a separate module,
+// I added them here so that they are contained within the module they apply
+// This is not how I'm used to having them, but since I was adding the routes
+// to an existing project I figured it made sense to try it this way
+const routes: Routes = [
+  { path: '**', redirectTo: '/people' },
+  { path: 'people', component: PeopleComponent },
+  // { path: '', redirectTo: '/people', pathMatch: 'full' },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/people/people.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-people',
+  templateUrl: './people.html',
+  styleUrls: ['./people.scss']
+})
+export class PeopleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
